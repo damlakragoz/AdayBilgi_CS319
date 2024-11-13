83aa6024d567bae707036477b96f418a52df1282
Erayisci <eray_isci@hotmail.com>
2024-11-13 20:04:44 +0300

    Initial commit

---
+++ Implementation/BTO_Management/src/main/java/com/cs319/project/BTO_management/BtoManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cs319.project.BTO_management;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtoManagementApplication {
+
+	public static void main(String[] args) {
+		System.out.println("BTO Management Application");
+		SpringApplication.run(BtoManagementApplication.class, args);
+	}
+
+}
---
+++ Implementation/BTO_Management/src/test/java/com/cs319/project/BTO_management/BtoManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs319.project.BTO_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtoManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
