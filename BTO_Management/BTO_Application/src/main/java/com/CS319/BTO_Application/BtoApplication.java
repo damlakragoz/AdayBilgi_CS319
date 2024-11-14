ef77ab1c617b15a658ba8b444d31f3af582bd412
Erayisci <eray_isci@hotmail.com>
2024-11-14 22:36:41 +0300

    application.properties

---
+++ BTO_Management/BTO_Application/src/main/java/com/CS319/BTO_Application/BtoApplication.java
@@ -0,0 +1,14 @@
+package com.CS319.BTO_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("BTO Application");
+		SpringApplication.run(BtoApplication.class, args);
+	}
+
+}
