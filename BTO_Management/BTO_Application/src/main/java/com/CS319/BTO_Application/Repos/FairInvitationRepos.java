a1bf0e23f1f808dcbe6e3bec3532f925d8ee82f9
kubracaglar <caglarhkubra@gmail.com>
2024-12-02 22:12:57 +0300

    application.properties

---
+++ BTO_Management/BTO_Application/src/main/java/com/CS319/BTO_Application/Repos/FairInvitationRepos.java
@@ -0,0 +1,8 @@
+package com.CS319.BTO_Application.Repos;
+
+import com.CS319.BTO_Application.Entity.FairInvitation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FairInvitationRepos extends JpaRepository<FairInvitation, Long> {
+}
+
