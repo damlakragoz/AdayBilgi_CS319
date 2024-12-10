47abec6fd6103aa2a6bafd703dccefc3faeca2d2
kubracaglar <caglarhkubra@gmail.com>
2024-12-10 17:37:38 +0300

    fair operations

---
+++ BTO_Management/BTO_Application/src/main/java/com/CS319/BTO_Application/DTO/FairOperationsForExecutive.java
@@ -0,0 +1,11 @@
+package com.CS319.BTO_Application.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FairOperationsForExecutive {
+    private Long fairId;
+    private String executiveName;
+}
