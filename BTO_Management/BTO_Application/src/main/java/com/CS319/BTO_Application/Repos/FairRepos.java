dfb93e23b4b8533cf9e71959367116fbcb9477df
kubracaglar <caglarhkubra@gmail.com>
2024-12-12 17:15:07 +0300

    fair files

---
+++ BTO_Management/BTO_Application/src/main/java/com/CS319/BTO_Application/Repos/FairRepos.java
@@ -0,0 +1,7 @@
+package com.CS319.BTO_Application.Repos;
+
+import com.CS319.BTO_Application.Entity.Fair;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FairRepos extends JpaRepository<Fair, Long> {
+}
