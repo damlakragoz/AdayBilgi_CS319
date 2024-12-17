d9106fd3d3ee697b433221033004377cb303aa74
Yigit <yigit.ozhan@ug.bilkent.edu.tr>
2024-12-17 19:29:21 +0300

    Puantage, submission of Tour Guide is done Advisor pages are added

---
+++ BTO_Application/319_Frontend/src/advisorpages/AdvisorHomepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdvisorHomepage = () => {
+    return (
+        <div className="advisor-homepage">
+            <h1>Welcome to the Advisor Dashboard</h1>
+            <p>Select an option from the sidebar to get started.</p>
+        </div>
+    );
+};
+
+export default AdvisorHomepage;
