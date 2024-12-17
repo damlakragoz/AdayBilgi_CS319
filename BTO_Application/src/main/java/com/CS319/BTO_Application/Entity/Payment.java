0b6873b96b06a540b8ee4c4d34770d8718e4c8eb
Emine Noor <114260503+eminenoor@users.noreply.github.com>
2024-12-17 17:52:08 +0300

    Coordinator approve payment functionality available

--- BTO_Application/src/main/java/com/CS319/BTO_Application/Entity/Payment.java
+++ BTO_Application/src/main/java/com/CS319/BTO_Application/Entity/Payment.java
@@ -28,9 +28,15 @@ public class Payment {
     @Column(name = "amount", nullable = false)
     private Double amount;
 
-    @Column(name = "payment_date", nullable = false)
-    private Date paymentDate;
+    @Column(name = "activity_submittion_date", nullable = false)
+    private Date activitySubmittionDate;
 
     @Column(name = "status", nullable = false)
     private String status; // PENDING, PAID, etc.
+
+    @Column(name = "approved_by")
+    private String approvedBy;
+
+    @Column(name = "approval_date")
+    private Date approvalDate;
 }
