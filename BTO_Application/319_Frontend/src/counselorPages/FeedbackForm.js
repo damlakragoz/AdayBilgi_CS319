e400c9c7a90be39e66474b729b7fc3509f270631
damlakaragoz <phoebesblogspot@gmail.com>
2024-12-19 02:56:42 +0300

    *Feedback form connected to backend, submittable

--- BTO_Application/319_Frontend/src/counselorPages/FeedbackForm.js
+++ BTO_Application/319_Frontend/src/counselorPages/FeedbackForm.js
@@ -1,37 +1,116 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./FeedbackForm.css";
 
-const FeedbackForm = ({ closeFeedback }) => {
+const FeedbackForm = ({ closeFeedback, tourId }) => {
     const [rating, setRating] = useState(0);
     const [feedbackDetails, setFeedbackDetails] = useState("");
     const [suggestions, setSuggestions] = useState("");
-    const [file, setFile] = useState(null);
+    const [tourDetails, setTourDetails] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-    const handleRating = (value) => setRating(value);
-
-    const handleFileChange = (event) => setFile(event.target.files[0]);
+    const timeSlotDisplayNames = {
+            SLOT_9_10: "09:00-10:00",
+            SLOT_10_11: "10:00-11:00",
+            SLOT_11_12: "11:00-12:00",
+            SLOT_13_14: "13:00-14:00",
+            SLOT_14_15: "14:00-15:00",
+        };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log({
-            rating,
-            feedbackDetails,
-            suggestions,
-            file,
-        });
-        alert("Geri bildirim başarıyla gönderildi!");
-        closeFeedback();
+        setLoading(true); // Disable the button
+
+        console.log(tourDetails.id);
+        console.log(rating);
+        console.log(suggestions);
+
+        const counselorEmail = localStorage.getItem("username");
+        console.log(counselorEmail);
+
+        const token = localStorage.getItem("userToken");
+        axios.post('http://localhost:8081/api/feedback/submit',
+            null,
+            {
+                params: {
+                    tourId: tourDetails.id,
+                    rating: rating,
+                    comment:  "Feedback: "+ feedbackDetails+ " Suggestions: "+ suggestions,
+                    counselorEmail: counselorEmail,
+                },
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json",
+                },
+                withCredentials: true,
+            })
+
+            .then((response) => {
+                alert("Geri bildirim başarıyla gönderildi!");
+                setLoading(false); // Enable the button
+                closeFeedback();
+            })
+            .catch((error) => {
+                console.error("Error submitting feedback:", error);
+                setLoading(false); // Enable the button
+            });
     };
+
+    // Fetch tour details when the component mounts
+    const fetchTourDetails = async (tourId) => {
+        try {
+            const token = localStorage.getItem("userToken");
+            // Log the necessary values to check
+            console.log("Tour ID:", tourId);
+
+            const response = await axios.get("http://localhost:8081/api/tour/getById", {
+                params: {
+                    tourId: tourId, // Pass the tourId as a query parameter
+                },
+                headers: {
+                    Authorization: `Bearer ${token}`, // Use the token for authorization if needed
+                    "Content-Type": "application/json",
+                },
+                withCredentials: true, // Include credentials if necessary
+            });
+
+            if (response.status === 200 || response.status === 201 || response.status === 202) {
+                console.log("Tour Details:", response.data);
+                // You can set the fetched data to your component state here
+                setTourDetails(response.data);
+            }
+        } catch (error) {
+            console.error("Error fetching tour details:", error);
+            alert("Etkinlik detayları alınırken bir hata oluştu.");
+        }
+    };
+
+    useEffect(() => {
+        if (tourId) {
+            fetchTourDetails(tourId);
+        }
+    }, [tourId]);
+
+    const formatDate = (date) => {
+            return new Date(date).toLocaleDateString("tr-TR");
+        };
+
+    const formatTimeSlot = (timeSlot) => {
+        return timeSlotDisplayNames[timeSlot] || timeSlot;
+    };
+
+    const handleRating = (value) => setRating(value);
 
     return (
         <div className="feedback-container shadow-sm">
             <h2 className="feedback-header">Etkinlik Geri Bildirimi</h2>
-            <div className="feedback-details">
-                <h5 className="details-title">Etkinlik Detayları</h5>
-                <p><strong>Tarih:</strong> 5 Kasım 2024</p>
-                <p><strong>Saat:</strong> 14:00 - 15:00</p>
-                <p><strong>Durum:</strong> Tamamlandı</p>
-            </div>
+            {tourDetails && (
+                <div className="feedback-details">
+                    <h5 className="details-title">Tamamlanan Turun Etkinlik Detayları</h5>
+                    <p><strong>Tarih:</strong> {formatDate(tourDetails.chosenDate)}</p>
+                    <p><strong>Saat:</strong> {formatTimeSlot(tourDetails.chosenTimeSlot)}</p>
+                </div>
+            )}
             <div className="rating">
                 <h5>Oy</h5>
                 <div className="stars-container">
@@ -46,33 +125,30 @@ const FeedbackForm = ({ closeFeedback }) => {
             </div>
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
-                    <label className="form-label">Detaylı Geri Bildirim</label>
+                    <label className="feedback-form-label">Detaylı Geri Bildirim</label>
                     <textarea
                         className="form-control"
                         value={feedbackDetails}
                         onChange={(e) => setFeedbackDetails(e.target.value)}
                         placeholder="Detaylı geri bildiriminizi buraya yazın..."
-                        rows="4"
+                        rows="3"
                     ></textarea>
                 </div>
                 <div className="form-group">
-                    <label className="form-label">Öneriler</label>
+                    <label className="feedback-form-label">Öneriler</label>
                     <textarea
                         className="form-control"
                         value={suggestions}
                         onChange={(e) => setSuggestions(e.target.value)}
                         placeholder="Herhangi bir öneriniz var mı?"
-                        rows="2"
+                        rows="3"
                     ></textarea>
                 </div>
-                <div className="form-group">
-                    <label className="form-label">Ekler</label>
-                    <input type="file" onChange={handleFileChange} className="form-control-file" />
-                    <small className="file-info">Kabul edilen dosya türleri: .jpg, .png, .pdf. Maksimum boyut: 5MB.</small>
-                </div>
                 <div className="form-actions">
-                    <button type="submit" className="btn-submit">Geri Bildirimi Yolla</button>
-                    <button type="button" onClick={closeFeedback} className="btn btn-secondary">Geri Bildirimlerime
+                    <button type="submit" className="btn-submit" disabled={loading}>
+                        {loading ? "Gönderiliyor..." : "Geri Bildirimi Yolla"}
+                    </button>
+                    <button type="button" onClick={closeFeedback} className="feedback-btn-secondary">Geri Bildirimlerime
                         Dön
                     </button>
                 </div>
--- BTO_Application/319_Frontend/src/counselorPages/GeriBildirimler.js
+++ BTO_Application/319_Frontend/src/counselorPages/GeriBildirimler.js
@@ -1,16 +1,173 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FeedbackForm from "./FeedbackForm";
+import axios from "axios";
 
 const GeriBildirimler = () => {
     const [showFeedbackForm, setShowFeedbackForm] = useState(false);
+    const [selectedTourId, setSelectedTourId] = useState(null); // State for the selected tour ID
+    const [tours, setTours] = useState([]);
+    const [feedbacks, setFeedbacks] = useState([]);
+    const [counselor, setCounselor] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    const feedbacks = [
-        { status: "Geri donüt bekleniyor", activity: "Tur", date: "25.08.2023", time: "11.30-12.30", stars: 0 },
-        { status: "Geri donüt verildi", activity: "Fuar", date: "20.07.2022", time: "15.30-16.30", stars: 3 },
-    ];
+    const timeSlotDisplayNames = {
+        SLOT_9_10: "09:00-10:00",
+        SLOT_10_11: "10:00-11:00",
+        SLOT_11_12: "11:00-12:00",
+        SLOT_13_14: "13:00-14:00",
+        SLOT_14_15: "14:00-15:00",
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await fetchCounselorInformation(); // Wait for counselor info
+            } catch (error) {
+                console.error("Error fetching counselor info:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        // Fetch tours only after the counselor state is updated
+        if (counselor?.schoolName) {
+            fetchTours();
+        }
+    }, [counselor]); // Dependency on counselor to trigger when it updates
+
+
+    const fetchCounselorInformation = async () => {
+        try {
+            const token = localStorage.getItem("userToken");
+            if (!token) {
+                alert("Authorization token missing. Please log in.");
+                return;
+            }
+
+            const resp = await axios.get("http://localhost:8081/api/counselors/getAll", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                withCredentials: true,
+            });
+
+            console.log("Counselors fetched:", resp.data);
+
+            const counselorEmail = localStorage.getItem("username")?.toLowerCase();
+//            if (!counselorEmail) {
+//                console.log("Counselor email is missing.");
+//
+//            }
+
+//            if (!Array.isArray(resp.data)) {
+//                console.log("Expected response data to be an array, but got:", typeof resp.data);
+//
+//            }
+
+            const filteredCounselors = resp.data.filter(
+                (counselor) => counselor.email?.toLowerCase() === counselorEmail
+            );
+
+            if (filteredCounselors.length > 0) {
+                setCounselor(filteredCounselors[0]); // Assuming you want the first match
+            } else {
+                console.warn("No counselor found with the provided email.");
+                setCounselor(null);
+            }
+        } catch (error) {
+            console.error("Error fetching counselors:", error);
+        }
+    };
+
+    const fetchTours = async () => {
+        try {
+            const token = localStorage.getItem("userToken");
+            if (!token) {
+                alert("Authorization token missing. Please log in.");
+                return;
+            }
+            console.log(counselor)
+
+            const response = await axios.get("http://localhost:8081/api/tour/getAll", {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                withCredentials: true,
+            });
+
+            // filter tours based on the school of counselor
+            let filteredTours = response.data.filter(
+                (tour) => tour.applyingHighschoolName === counselor.schoolName
+            );
+            filteredTours = filteredTours.filter(
+                (tour) => tour.tourStatus != "Rejected" && tour.tourStatus != "Cancelled"
+            );
+
+            console.log(filteredTours);
+
+            const sortedTours = filteredTours.sort((a, b) => {
+                const dateA = new Date(a.assignedDate);
+                const dateB = new Date(b.assignedDate);
+                return dateB - dateA; // Descending order
+            });
+
+            setTours(sortedTours);
+        } catch (error) {
+            console.error("Error fetching tours:", error);
+            alert("Failed to fetch tours. Please try again later.");
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const fetchFeedbacks = async () => {
+        try {
+            const token = localStorage.getItem("userToken");
+            if (!token) {
+                alert("Authorization token missing. Please log in.");
+                return;
+            }
+            const response = await axios.get("http://localhost:8081/api/feedback/getAll", {
+                headers: { Authorization: `Bearer ${token}` },
+                withCredentials: true,
+            });
+
+            setFeedbacks(response.data);
+        } catch (error) {
+            console.error("Error fetching feedbacks:", error);
+        }
+    };
+
+    const hasFeedbackForTour = (tourId) => {
+        return feedbacks.some((feedback) => feedback.tourId === tourId);
+    };
+
+    const formatDate = (date) => {
+        return new Date(date).toLocaleDateString("tr-TR");
+    };
+
+    const formatTimeSlot = (timeSlot) => {
+        return timeSlotDisplayNames[timeSlot] || timeSlot;
+    };
+
+    const handlePenClick = (tourId) => {
+        setSelectedTourId(tourId); // Set the selected tour ID
+        setShowFeedbackForm(true); // Show the feedback form
+    };
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
 
     if (showFeedbackForm) {
-        return <FeedbackForm closeFeedback={() => setShowFeedbackForm(false)} />;
+        return (
+            <FeedbackForm
+                closeFeedback={() => setShowFeedbackForm(false)}
+                tourId={selectedTourId} // Pass the selected tour ID to the FeedbackForm
+            />
+        );
     }
 
     return (
@@ -18,39 +175,48 @@ const GeriBildirimler = () => {
             <h2>Geri Bildirimlerim</h2>
             <table className="table">
                 <thead>
-                <tr>
-                    <th>Durum</th>
-                    <th>Aktivite</th>
-                    <th>Tarih</th>
-                    <th>Saat</th>
-                    <th>Oy</th>
-                    <th>Geri Bildirim</th>
-                </tr>
+                    <tr>
+                        <th>Tur Durumu</th>
+                        <th>Geri Bildirim</th>
+                        <th>Tarih</th>
+                        <th>Saat</th>
+                        <th>Oy</th>
+                        <th>Düzenle</th>
+                    </tr>
                 </thead>
                 <tbody>
-                {feedbacks.map((feedback, index) => (
-                    <tr key={index}>
-                        <td>{feedback.status}</td>
-                        <td>{feedback.activity}</td>
-                        <td>{feedback.date}</td>
-                        <td>{feedback.time}</td>
-                        <td>
-                            {[...Array(5)].map((_, i) => (
+                    {tours.map((tour) => (
+                        <tr key={tour.id}>
+                            <td>{tour.tourStatus}</td>
+                            <td>
+                                {hasFeedbackForTour(tour.id)
+                                    ? "Geri dönüş verildi"
+                                    : "Geri dönüşünüz bekleniyor"}
+                            </td>
+                            <td>{formatDate(tour.chosenDate)}</td>
+                            <td>{formatTimeSlot(tour.chosenTimeSlot)}</td>
+                            <td>
+                                {[...Array(5)].map((_, i) => (
+                                    <i
+                                        key={i}
+                                        className={`fas fa-star ${
+                                            i < tour.stars ? "text-warning" : "text-secondary"
+                                        }`}
+                                    ></i>
+                                ))}
+                            </td>
+                            <td>
                                 <i
-                                    key={i}
-                                    className={`fas fa-star ${i < feedback.stars ? "text-warning" : "text-secondary"}`}
+                                    className="fas fa-pen text-primary"
+                                    onClick={() => handlePenClick(tour.id)} // Pass the tour ID
+                                    style={{
+                                        color: '#2c7a7b',
+                                        cursor: 'pointer',
+                                      }}
                                 ></i>
-                            ))}
-                        </td>
-                        <td>
-                            <i
-                                className="fas fa-pen text-primary"
-                                onClick={() => setShowFeedbackForm(true)}
-                                style={{ cursor: "pointer" }}
-                            ></i>
-                        </td>
-                    </tr>
-                ))}
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
